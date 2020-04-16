c015e06935b0b7eaf1efd4a3f2dfb98d7ef899ef
Senthilkumar Devaraj <dsk90it@gmail.com>
2020-04-16 14:27:54 +0530

    destructuring tutorial lip

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
                 loader: 'babel-loader',
                 options: {
                     presets: ['@babel/preset-env'],
+                    plugins: ['transform-object-rest-spread']
                 }
             }
         }]
